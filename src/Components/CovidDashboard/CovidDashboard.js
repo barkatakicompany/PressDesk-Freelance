c30038cd0050c464f2412b36f61380084e6fb430
rekib <rkb.ra0025@gmail.com>
2020-12-09 22:53:26 +0530

    covid clock style

--- src/Components/CovidDashboard/CovidDashboard.js
+++ src/Components/CovidDashboard/CovidDashboard.js
@@ -84,26 +84,105 @@ export default function CovidDashboard() {
   return isLoaded ? (
     <div className="m-3 dashboard-wrapper">
       <div className="border covid-dashboard">
-        <div className="mr-auto">Covid-19 in India</div>
-        <div style={{backgroundColor: "#dadada"}}>
-          <div>Total Cases</div>
-          <div>{covidCases.total_cases}</div>
-          <div>{covidCases.cases_diff}</div>
+        <div className="mr-auto mt-auto mb-auto">
+          <div className="text-bold-big text-red">Covid-19 in India</div>
+          <div className="text-italic-small mt-2">Source: MoHFW</div>
         </div>
-        <div style={{backgroundColor: "#cde6fd"}}>
-          <div>Total Active</div>
-          <div>{covidCases.total_active}</div>
-          <div>{covidCases.active_diff}</div>
+        <div style={{ backgroundColor: "#dadada" }} className="text-bold">
+          <div style={{ color: "" }}>Total Cases</div>
+          <div className="text-bold-big">{covidCases.total_cases}</div>
+          <div className="text-red">
+            <span className="p-1">
+              <svg
+                class="ctw_statsRMarker"
+                width="8"
+                height="5"
+                viewBox="0 0 8 5"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M0.25 4.5L4 0.75L7.75 4.5H0.25Z"
+                  fill="#D10014"
+                ></path>
+              </svg>
+            </span>
+            {covidCases.cases_diff}
+          </div>
         </div>
-        <div style={{backgroundColor: "#cdeadb"}}>
-          <div>Total Recovered</div>
-          <div>{covidCases.total_recovered}</div>
-          <div>{covidCases.recovered_diff}</div>
+        <div style={{ backgroundColor: "#cde6fd" }} className="text-bold">
+          <div style={{ color: "#005ebb" }}>Total Active</div>
+          <div className="text-bold-big">{covidCases.total_active}</div>
+          <div className="text-red">
+            <span className="p-1">
+              <svg
+                class="ctw_statsRMarker"
+                width="8"
+                height="5"
+                viewBox="0 0 8 5"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M0.25 4.5L4 0.75L7.75 4.5H0.25Z"
+                  fill="#D10014"
+                ></path>
+              </svg>
+            </span>
+            {covidCases.active_diff}
+          </div>
         </div>
-        <div style={{backgroundColor: "#f6ccd0"}}>
-          <div>Total Deaths</div>
-          <div>{covidCases.total_deaths}</div>
-          <div>{covidCases.deaths_diff}</div>
+        <div style={{ backgroundColor: "#cdeadb" }} className="text-bold">
+          <div style={{ color: "#03592e" }}>Total Recovered</div>
+          <div className="text-bold-big">{covidCases.total_recovered}</div>
+          <div style={{ color: "#03592e" }}>
+            <span className="p-1">
+              <svg
+                class="ctw_statsRMarker"
+                width="8"
+                height="5"
+                viewBox="0 0 8 5"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M0.25 4.5L4 0.75L7.75 4.5H0.25Z"
+                  fill="#03592E"
+                ></path>
+              </svg>
+            </span>
+            {covidCases.recovered_diff}
+          </div>
+        </div>
+        <div style={{ backgroundColor: "#f6ccd0" }} className="text-bold">
+          <div className="text-red">Total Deaths</div>
+          <div className="text-bold-big">{covidCases.total_deaths}</div>
+          <div className="text-red">
+            <span className="p-1">
+              <svg
+                class="ctw_statsRMarker"
+                width="8"
+                height="5"
+                viewBox="0 0 8 5"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M0.25 4.5L4 0.75L7.75 4.5H0.25Z"
+                  fill="#D10014"
+                ></path>
+              </svg>
+            </span>
+            {covidCases.deaths_diff}
+          </div>
         </div>
       </div>
     </div>
