6eea00b418be8f651fefc5b951cea1b8bd273fed
rekib <rkb.ra0025@gmail.com>
2020-12-13 19:03:40 +0530

    footer

--- src/Components/Base.js
+++ src/Components/Base.js
@@ -1,17 +1,21 @@
 import React from "react";
+import Footer from "../Footer";
 import Menu from "../Menu";
 import "../styles.css";
 const Base = ({ children }) => {
   return (
     <div className="">
       <Menu />
-      <div className="pt-3 p-3">{children}</div>
-      <div
-        id="loading"
-        className="loading-screen row m-0 p-0 h-100 w-100 align-items-center justify-content-center d-none"
-      >
-        <div class="loader"></div>
+      <div style={{minHeight: "100vh"}}>
+        <div className="pt-3 p-3">{children}</div>
+        <div
+          id="loading"
+          className="loading-screen row m-0 p-0 h-100 w-100 align-items-center justify-content-center d-none"
+        >
+          <div class="loader"></div>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
---
+++ src/Footer.js
@@ -0,0 +1,117 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="" style={{ height: "20vh" }}>
+      <br />
+      <div className="my-container">
+        <div className=" row pt-5 text-black-75">
+          <div className="col-md-3">
+            <h2 className="text-red text-bold-big">PressDesk.in</h2>
+            <p>
+              <span className="text-red text-bold-small">
+                Corporate Office:
+              </span>{" "}
+              <br />
+              Barkataki Company Barkataki Market, <br />
+              A. T. Road, Jorhat <br /> Jorhat-785001 Assam, India
+            </p>
+          </div>
+          <div className="col-md-3">
+            <p>
+              <span className="text-red text-bold-small">
+                Editorial &amp; Syndication issues
+              </span>{" "}
+              <br /> In case of any issues regarding the content on the site
+              please mail us at: <br />
+              <a
+                href="mailto:editor@pressdesk.in"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                editor@pressdesk.in
+              </a>
+            </p>
+            <p>
+              <span className="text-red text-bold-small">Business queries</span>
+              <br /> To reach out to us for any business related queries please
+              mail us at: <br />
+              <a
+                href="mailto:editor@pressdesk.in"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                info@pressdesk.in
+              </a>
+            </p>
+          </div>
+          <div className="col-md-3">
+            <p>
+              <span className="text-red text-bold-small">
+                Editorial Office:{" "}
+              </span>
+              <br />
+              House No. 10, <br />
+              Purbanchal Path, <br />
+              Near Sun Valley Hospital, <br />
+              Bormotoria, <br />
+              Guwahati-781006 <br />
+              Website –
+              <a href="#" className="footer-link">
+                www.pressdesk.in <br />
+              </a>
+              Email id-{" "}
+              <a
+                href="mailto:editor@pressdesk.in"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                editor@nenow.in
+              </a>{" "}
+              <br />
+              Contact No- 0361-3561270
+            </p>
+          </div>
+          <div className="col-md-3">
+            <a href="#" className="footer-list">
+              About Us
+            </a>{" "}
+            <br />
+            <a href="#" className="footer-list">
+              Adveritse
+            </a>{" "}
+            <br />
+            <a href="#" className="footer-list">
+              Careers
+            </a>{" "}
+            <br />
+            <a href="#" className="footer-list">
+              Intern With Us
+            </a>{" "}
+            <br />
+            <a href="#" className="footer-list">
+              Contact Us
+            </a>
+          </div>
+        </div>
+        <br />
+        <div>
+          <a
+            href="#"
+            className="d-flex justify-content-center align-items-center"
+          >
+            <img
+              src={require("./static/images/logo.png")}
+              alt="LOGO"
+              style={{ width: "60px" }}
+            />
+            <span className="text-bold-big" style={{ fontSize: "3rem" }}>
+              PressDesk.in
+            </span>
+          </a>
+        </div>
+        <hr />
+      </div>
+    </div>
+  );
+}
